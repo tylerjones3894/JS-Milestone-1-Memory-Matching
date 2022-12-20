4136650187f6695f0d392d23381bb71528b71cac
Tyler Jones <tylerjones3894@gmail.com>
2022-12-19 18:36:31 -0800

    Commented out all game code I've typed. I need to take a different approach, but want my original code for reference.

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+// const array = [{
+//     imgSrc: "image path", index: 0,
+// },
+// {
+//     imgSrc: "image path", index: 1
+// },
+// {
+//     imgSrc: "image path", index: 2
+// },
+// {
+//     imgSrc: "image path", index: 3
+// },
+// {
+//     imgSrc: "image path", index: 4
+// },
+// {
+//     imgSrc: "image path", index: 5
+// },
+// {
+//     imgSrc: "image path", index: 6
+// },
+// {
+//     imgSrc: "image path", index: 7
+// }]
+
+// var image1 = document.createElement("img");
+// const element = document.getElementById("#card1")
+// element.appendChild(image1)
+// image1.src=array[0].imgSrc
