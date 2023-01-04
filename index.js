a721e5d9fcc702b202cd8498af5e5c1b5c321e31
Tyler Jones <tylerjones3894@gmail.com>
2023-01-03 22:22:10 -0800

    Added the functions to check for matches and unflip when matches are false. This was done using data-framework.

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
 const cards = document.querySelectorAll('.memoryCard');
 
-let hasFlippedCard
+let hasFlippedCard = false
+let lockBoard = false;
+let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+
     this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+
+        return;
+    } 
+        hasFlippedCard = false;
+        secondCard = this;
+
+        checkForMatch()
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards()
+    } else {
+        unflipCards()
+    }
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard)
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        lockBoard = false;
+        }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
-
-// const array = [{
-//     imgSrc: "image path", index: 0,
-// },
-// {
-//     imgSrc: "image path", index: 1
-// },
-// {
-//     imgSrc: "image path", index: 2
-// },
-// {
-//     imgSrc: "image path", index: 3
-// },
-// {
-//     imgSrc: "image path", index: 4
-// },
-// {
-//     imgSrc: "image path", index: 5
-// },
-// {
-//     imgSrc: "image path", index: 6
-// },
-// {
-//     imgSrc: "image path", index: 7
-// }]
-
-// var image1 = document.createElement("img");
-// const element = document.getElementById("#card1")
-// element.appendChild(image1)
-// image1.src=array[0].imgSrc
